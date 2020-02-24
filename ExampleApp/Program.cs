1946ff12ba29fd3dcdc429b67b9c8ce7d163402b
Anatoly Buranov <tolyandre@yandex.ru>
2020-02-24 21:50:09 +0400

    Initial commit

---
+++ ExampleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using Serilog;
+using Serilog.Context;
+using Serilog.Formatting.Compact;
+using Serilog.FromExceptionContextEnricher;
+using System;
+
+namespace ExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+             .Enrich.FromLogContext()
+             .Enrich.With<FromExceptionContextEnricher>()
+
+             .WriteTo.Console(new RenderedCompactJsonFormatter())
+             .CreateLogger();
+
+            Example1();
+
+            Log.CloseAndFlush();
+        }
+
+        private static void Example1()
+        {
+            using (LogContext.PushProperty("MyName", "Example1"))
+                try
+                {
+                    using (LogContext.PushProperty("MyName", "Inside TRY in Example1"))
+                    using (LogContext.PushProperty("Local", "Value from origin context"))
+                    {
+                        throw new InvalidOperationException("test");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex, "Exception caught in MakeLogs()");
+                }
+        }
+    }
+}
---
+++ Serilog.FromExceptionContextEnricher/FromExceptionContextEnricher.cs
@@ -0,0 +1,46 @@
+﻿using Serilog.Context;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+
+namespace Serilog.FromExceptionContextEnricher
+{
+    public class FromExceptionContextEnricher : ILogEventEnricher
+    {
+        static readonly ConditionalWeakTable<Exception, List<ILogEventEnricher>> ConditionalWeakTable =
+            new ConditionalWeakTable<Exception, List<ILogEventEnricher>>();
+
+        static FromExceptionContextEnricher()
+        {
+            AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+        }
+
+        private static void CurrentDomain_FirstChanceException(object sender, FirstChanceExceptionEventArgs e)
+        {
+            var exceptionContexts = ConditionalWeakTable.GetValue(e.Exception, _ => new List<ILogEventEnricher>());
+            exceptionContexts.Add(LogContext.Clone());
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            Exception exception = logEvent.Exception;
+
+            while (exception != null)
+            {
+                if (ConditionalWeakTable.TryGetValue(exception, out List<ILogEventEnricher> contexts))
+                {
+                    foreach (var context in contexts.AsEnumerable().Reverse())
+                    {
+                        context.Enrich(logEvent, propertyFactory);
+                    }
+                }
+
+                exception = exception.InnerException;
+            }
+        }
+    }
+}
